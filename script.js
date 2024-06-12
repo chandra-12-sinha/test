6f76b4d6009907939859bb7f9cd095d0c1b9f279
chandra-12-sinha <chandrakalasinha083@gmail.com>
2024-06-12 13:00:54 +0530

    while loop

--- script.js
+++ script.js
@@ -3,4 +3,12 @@ function add(a,b){
 console.log(a-b)
 }
 add(2,5)
-for(let i = 0; i<=Array.length; i++){}
+for(let i = 0; i<=Array.length; i++){
+return i++
+}
+
+let cn = 2
+while(i!= cn){
+  console.log("currect number")
+}
+console.log("enter number id not a currect number")
